21d6c9eb1f3755576f1907aa37d1342ca9b94a31
deventorum <deventorum@gmail.com>
2018-12-03 21:12:54 +0000

    Game shows the winner

--- game.rb
+++ game.rb
@@ -8,8 +8,6 @@ class Game
 
   def start
     puts "New Game Started. Welcome #{player_1.name} and #{player_2.name}"
-    pp @player_1
-    pp @player_2
     turn
   end
 
@@ -17,16 +15,28 @@ class Game
     puts "P1: #{@player_1.lives}/3 vs P2: #{@player_2.lives}/3"
   end
 
+  def winner(player)
+    puts "#{player.name} wins with the score of #{player.lives}/3"
+    puts '---------GAME-OVER--------'
+    puts 'Good bye!'
+    exit(0)
+  end
+
+  def check_score
+    if @player_1.lives == 0
+      winner(player_2)
+    elsif @player_2.lives == 0
+      winner(player_1)
+    end
+  end
+
   def turn
     @player_1.new_question
+    check_score
     @player_2.new_question
-    if @player_1.lives > 0 && @player_2.lives > 0
-      show_stats
-      puts '---------NEW-TURN---------'
-      turn
-    else
-      puts '---------GAME-OVER--------'
-      puts 'Good bye!'
-    end
+    check_score
+    show_stats
+    puts '---------NEW-TURN---------'
+    turn
   end
 end
